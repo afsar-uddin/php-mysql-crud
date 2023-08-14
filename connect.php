ebfce9148da5cf8f1406e41a8f95581970025976
afsar-uddin <afsarwebdev@gmail.com>
2023-08-14 08:07:56 +0600

    data inserted

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$con = new mysqli('localhost', 'root', '', 'php-mysql-crud'); 
+
+if(!$con) {
+    die(mysqli_error($con)); 
+}
+
+
+
+
+?>
---
+++ users.php
@@ -0,0 +1,43 @@
+<?php
+    include 'connect.php';
+
+    if(isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $mobile = $_POST['mobile'];
+        $password = $_POST['password'];
+
+        $sql = "INSERT INTO `users` (name, email, mobile, password) VALUES ('$name','$email','$mobile','$password')";
+        $user_query = mysqli_query($con, $sql);
+        if($user_query) {
+            echo "Data inserted successfully";
+        } else {
+            die(mysqli_error($con));
+        }
+    }
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+</head>
+<body>
+    <div class="nwt-users">
+        <form method="post">
+            <input type="text" name="name" placeholder="Your Name">
+            <input type="email" name="email" placeholder="Your email">
+            <input type="text" name="mobile" placeholder="Your Mobile Number">
+            <input type="text" name="password" placeholder="Your password">
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </div>
+</body>
+</html>
